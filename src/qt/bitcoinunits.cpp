d6a5cc6e7c6f3d00f4aec378618319e71db16471
yungcloud <info@yungcloud.com>
2017-05-18 00:07:05 -0700

    first commit

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("yng");
+    case mBTC: return QString("myng");
+    case uBTC: return QString::fromUtf8("μyng");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("cloudcoin");
+    case mBTC: return QString("Milli-cloudcoin (1 / 1,000)");
+    case uBTC: return QString("Micro-cloudcoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
