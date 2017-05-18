280402b950320da6d4c54c43e74ac40a29b8f400
yungcloud <info@yungcloud.com>
2017-05-18 09:10:26 -0700

    port numbers & math

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 62, // addresses start with S
+        PUBKEY_ADDRESS = 56, // addresses start with Y
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
